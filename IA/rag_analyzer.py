9eba394858292221e20d0f972a25d6f583dd309e
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-08-13 12:00:14 -0300

    Update rag_analyzer.py

--- IA/rag_analyzer.py
+++ IA/rag_analyzer.py
@@ -60,7 +60,7 @@ class RAGAnalyzer:
                 HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
                 HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
             }
-            self.model = genai.GenerativeModel('gemini-2.5-pro')
+            self.model = genai.GenerativeModel('gemini-2.5-flash')
         except Exception as e:
             st.error(f"Falha ao configurar o modelo Gemini: {e}")
             st.stop()
